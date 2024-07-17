83999ba263e27021867a120dc6445366af2ba1df
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-07-17 09:54:59 +0300

    cleaning the code

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,5 +1,5 @@
 import Signup from "./page/auth/Signup";
-import Signin from "./page/auth/signin";
+import Signin from "./page/auth/Signin";
 import AddProduct from "./page/product/AddProduct";
 import Product from "./page/product/Product";
 import ProductList from "./page/product/ProductList";
